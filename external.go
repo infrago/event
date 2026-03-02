03a19b0e5c646fe7fb41de2fec058dbef0ba542a
yatniel <yatniel@qq.com>
2026-03-02 10:01:41 -0800

    优化http/web执行

--- external.go
+++ external.go
@@ -17,15 +17,3 @@ func Broadcast(name string, values ...Map) error {
 func BroadcastTo(conn, name string, values ...Map) error {
 	return module.broadcast(conn, name, values...)
 }
-
-func RegisterDriver(name string, driver Driver) {
-	module.RegisterDriver(name, driver)
-}
-
-func RegisterConfig(name string, cfg Config) {
-	module.RegisterConfig(name, cfg)
-}
-
-func RegisterConfigs(cfgs Configs) {
-	module.RegisterConfigs(cfgs)
-}
